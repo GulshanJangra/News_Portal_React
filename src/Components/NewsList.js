a00acb24b658d55f6865995d9b2a06a54e804fbe
Gulshan Jangra <gulshanjangra130@gmail.com>
2024-06-22 17:50:26 +0530

    deployed website

--- src/Components/NewsList.js
+++ src/Components/NewsList.js
@@ -7,7 +7,7 @@ import CustomPagination from "./CustomPagination";
 const NewsList = (props) => {
   const { category, searchTerm } = props;
   const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 4;
+  const pageSize = 6;
 
   const onPageChange = (pageNumber) => setCurrentPage(pageNumber);
 
